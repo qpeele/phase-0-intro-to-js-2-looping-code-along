489d31d4643eb3faf9002b29b2b715362151a0c8
Quachell Peele <quachellpeele@gmail.com>
2023-10-29 21:31:59 -0400

    i am done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    let countdown = number;
+    while (countdown >= 0) {
+      console.log(countdown);
+      countdown--;
+    }
+  }
+  
+  countDown(5);
+  
